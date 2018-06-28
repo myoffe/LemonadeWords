25014e96fd1dd9cdf650ddfda21a42b76ff99337
Michael Yoffe <myoffe@gmail.com>
2018-06-28 18:56:55 +0300

    basic word counter

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
-from flask import Flask
+import re
+from collections import Counter
+
+from flask import Flask, request
+
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route('/')
 def hello():
-    return "Hello World!"
+    return "Hello Lemonade!"
+
+
+@app.route('/count', methods=['POST'])
+def count():
+    text = request.json['text']
+    words = get_words(text)
+    freqs = Counter(words)
+    return frequencies_string(freqs)
+
+
+def frequencies_string(freqs):
+    return '\n'.join([f'{word}: {times}' for word, times in freqs.items()])
+
+
+def get_words(s):
+    return re.sub('\W', ' ', s).lower().split()
